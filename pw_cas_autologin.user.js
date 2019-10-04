8ad81a9911dad91c6a3191f6ad5aeb0826dbd319
skalermo <34585360+skalermo@users.noreply.github.com>
2019-10-04 12:14:47 +0200

    Update pw_cas_autologin.user.js

--- pw_cas_autologin.user.js
+++ pw_cas_autologin.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name     PW CAS Autologin
 // @description     Go through annoying 'Ukryj identyfikator' and autoclick 'Zaloguj'
-// @version  1
+// @version  1.1
 // @grant    none
-// @namespace  https://cas.usos.pw.edu.pl/cas/login
+// @include  https://cas.usos.pw.edu.pl/cas/login*
 // ==/UserScript==
 
 document.getElementById("vis").checked = false;
@@ -12,4 +12,5 @@ document.getElementById("username").type = "text";
 // If you web browser doesn't fill in the form automatically you may need to uncomment this line
 // document.getElementById("username").value = "your_login";
 
-setTimeout(function() {document.getElementsByClassName("btn-submit")[0].click();}, 5);
+// Feel free to set other timeout
+setTimeout(function() {document.getElementsByClassName("btn-submit")[0].click();}, 20);
