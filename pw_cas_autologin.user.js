d1c69a973b31cc753eb92658e75b36b20973f86a
skalermo <34585360+skalermo@users.noreply.github.com>
2019-10-04 12:00:03 +0200

    Create pw_cas_autologin.user.js

---
+++ pw_cas_autologin.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name     PW CAS AUTOLOGIN
+// @description     Go through annoying 'Ukryj identyfikator' and autoclick 'Zaloguj'
+// @version  1
+// @grant    none
+// @namespace  https://cas.usos.pw.edu.pl/cas/login
+// ==/UserScript==
+
+document.getElementById("vis").checked = false;
+document.getElementById("username").type = "text";
+setTimeout(function() {document.getElementsByClassName("btn-submit")[0].click();}, 5);
